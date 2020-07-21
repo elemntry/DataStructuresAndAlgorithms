f593d0ff570794814cc2151e7bbd18f12cee6423
elemntry <xarradex@gmail.lcom>
2020-07-21 10:34:05 +0500

    Add project files.

---
+++ AlgorithmicToolbox/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AlgorithmicToolbox
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
