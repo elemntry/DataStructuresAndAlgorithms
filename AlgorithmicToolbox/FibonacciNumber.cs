5144b95427fc348bb1a45d69452d81fae859cd6d
elemntry <xarradex@gmail.lcom>
2020-07-21 19:58:48 +0500

    add calculate fibonacci algo

---
+++ AlgorithmicToolbox/FibonacciNumber.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmicToolbox
+{
+    class FibonacciNumber
+    {
+        public static int FibonacciNaive(int n)
+        {
+            if (n <= 1) return n;
+            return FibonacciNaive(n - 1) + FibonacciNaive(n - 2);
+        }
+        public static int FibonacciFast (int n)
+        {
+            int a = 1;
+            int b = 1;
+            for (int i = 3; i <= n; i++)
+            {
+                int c = a + b;// compute next fibonacci number
+                a = b;
+                b = c;
+            }
+            return b;
+        }
+    }
+}
