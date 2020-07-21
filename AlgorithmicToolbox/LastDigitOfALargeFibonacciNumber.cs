f1415c361597fdf863d4228fddd82944e6a8b73d
elemntry <xarradex@gmail.lcom>
2020-07-21 19:59:09 +0500

    add last digit of fiibonacci number algo

---
+++ AlgorithmicToolbox/LastDigitOfALargeFibonacciNumber.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmicToolbox
+{
+    class LastDigitOfALargeFibonacciNumber
+    {
+        public static int FibonacciLastDigit(int n)
+        {
+            return FibonacciNumber.FibonacciFast(n) % 10;
+        }
+    }
+}
