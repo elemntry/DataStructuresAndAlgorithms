d6885a0d8585556b35572bbed3a91811532d36bb
elemntry <xarradex@gmail.lcom>
2020-07-21 15:40:07 +0500

    clean solution

--- AlgorithmicToolbox/MaximumPairwiseProduct.cs
+++ AlgorithmicToolbox/MaximumPairwiseProduct.cs
@@ -9,18 +9,11 @@ namespace AlgorithmicToolbox
     {
         public static void MPD()
         {
+            //read qty elements of array
             var val = Convert.ToInt32(Console.ReadLine());
-            var values = Console.ReadLine().Split(" ").Select(s => double.Parse(s)).OrderByDescending(el => el).ToArray();            
-            double result = 0;
-            if (values.Length > 1) result = values[0] * values[1];
-            for (int i = 2; i < values.Length; i++)
-            {
-                for (int j = i+1; j < values.Length; j++)
-                {
-                    if(result % values[i] != 0) result *= values[j];
-                }
-            }
-            Console.WriteLine(result);
+            //read from string, create and sort array
+            var values = Console.ReadLine().Split(' ').Select(s => double.Parse(s)).OrderByDescending(el => el).ToArray();
+            Console.WriteLine(values[0] * values[1]);
         }
     }
 }
--- AlgorithmicToolbox/Program.cs
+++ AlgorithmicToolbox/Program.cs
@@ -7,18 +7,7 @@ namespace AlgorithmicToolbox
     {
         static void Main()
         {
-            var val = Convert.ToInt32(Console.ReadLine());
-            var values = Console.ReadLine().Split(" ").Select(s => double.Parse(s)).OrderByDescending(el => el).ToArray();
-            //double result = 0;
-            //if (values.Length > 1) result = values[0] * values[1];
-            //for (int i = 2; i < values.Length; i++)
-            //{
-            //    for (int j = i + 1; j < values.Length; j++)
-            //    {
-            //        if (result % values[i] != 0) result *= values[j];
-            //    }
-            //}
-            Console.WriteLine(values[0]*values[1]);
+
         }
     }
 }
