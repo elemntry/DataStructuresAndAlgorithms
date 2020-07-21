49713264ba8f2c946929a9243377f53ff5b78af0
elemntry <xarradex@gmail.lcom>
2020-07-21 19:59:37 +0500

    wip GCD

---
+++ AlgorithmicToolbox/GreatestCommonDivisor.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AlgorithmicToolbox
+{
+    class GreatestCommonDivisor
+    {
+        public int GCD(int a, int b)
+        {
+            
+        }
+    }
+}
