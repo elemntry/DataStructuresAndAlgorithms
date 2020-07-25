fa18de482d21b4735c0a66556389ccfa27af98dc
elemntry <xarradex@gmail.com>
2020-07-25 09:30:55 +0500

    code cleanup

--- AlgorithmicToolbox/Program.cs
+++ AlgorithmicToolbox/Program.cs
@@ -9,7 +9,6 @@ namespace AlgorithmicToolbox
     {
         static void Main()
         {
-
             InputForGetOptimalValue();
         }
 
@@ -19,9 +18,11 @@ namespace AlgorithmicToolbox
             //read qty elements of array
             var val = Convert.ToInt32(Console.ReadLine());
             //read from string, create and sort array
-            var values = Console.ReadLine().Split(' ').Select(s => double.Parse(s)).OrderByDescending(el => el).ToArray();
+            var values = Console.ReadLine().Split(' ').Select(s => double.Parse(s)).OrderByDescending(el => el)
+                .ToArray();
             Console.WriteLine(values[0] * values[1]);
         }
+
         /*
         Fibonacci numbers
         */
@@ -31,7 +32,7 @@ namespace AlgorithmicToolbox
             return FibonacciNaive(n - 1) + FibonacciNaive(n - 2);
         }
 
-        static double FibonacciFast (int n)
+        static double FibonacciFast(int n)
         {
             if (n == 0) return 0;
             if (n == 1) return 1;
@@ -49,13 +50,16 @@ namespace AlgorithmicToolbox
             double a = 1;
             double b = 1;
             for (var i = 3; i <= n; i++)
-            checked {
-                var c = (a + b);// compute next fibonacci number
-                a = b;
-                b = c;
-            }
+                checked
+                {
+                    var c = (a + b); // compute next fibonacci number
+                    a = b;
+                    b = c;
+                }
+
             return b;
         }
+
         /*
         Last digit of a large fibonacci number
         */
@@ -68,12 +72,14 @@ namespace AlgorithmicToolbox
             int b = 1;
             for (var i = 3; i <= n; i++)
             {
-                    var c = (a + b) % 10;// compute next fibonacci number
-                    a = b;
-                    b = c;
+                var c = (a + b) % 10; // compute next fibonacci number
+                a = b;
+                b = c;
             }
+
             return b;
         }
+
         /*
         Greatest common divisor
         */
@@ -85,8 +91,10 @@ namespace AlgorithmicToolbox
             {
                 if (a % d == 0 && b % d == 0) gcd = d;
             }
+
             return gcd;
         }
+
         static int EuclidGCD(int a, int b)
         {
             int gcd = 1;
@@ -96,45 +104,52 @@ namespace AlgorithmicToolbox
             if (b >= a) return EuclidGCD(a, b % a);
             return gcd;
         }
+
         /*
         Least Common Multiple 
         */
         static double LCM(int a, int b)
         {
-            return Math.Round((double)a * (double)b / EuclidGCD(a, b));
+            return Math.Round((double) a * (double) b / EuclidGCD(a, b));
         }
+
         /*
         Money Change 
         */
         static int MoneyChange(int m)
         {
-            int[] denominationsCoins = new[] { 1, 5, 10 };
+            int[] denominationsCoins = new[] {1, 5, 10};
             int NumberOfCoins = 0;
             int i = denominationsCoins.Length - 1;
-            while(i >= 0)
+            while (i >= 0)
             {
-                while(m >= denominationsCoins[i])
+                while (m >= denominationsCoins[i])
                 {
-                    m -= denominationsCoins[i];                    
+                    m -= denominationsCoins[i];
                     NumberOfCoins++;
                 }
+
                 i--;
             }
 
             return NumberOfCoins;
         }
+
         /*
         Maximum Value of the Loot 
         //WORK IN PROGRESS
         // DO: sort array bt val/weight before start
         */
-        static double getOptimalValue(double capacity, List<int[]> valsWeights)
+        static double GetOptimalValue(double capacity, List<int[]> valsWeights)
         {
             double value = 0;
-            if (valsWeights.Count == 1) return capacity > valsWeights.ElementAt(0)[0] ? valsWeights.ElementAt(0)[0] : Math.Round(capacity / valsWeights.ElementAt(0)[1] * valsWeights.ElementAt(0)[0], 4);            
-            while (capacity > 0)
+            if (valsWeights.Count == 1)
+                return capacity > valsWeights.ElementAt(0)[0]
+                    ? valsWeights.ElementAt(0)[0]
+                    : Math.Round(capacity / valsWeights.ElementAt(0)[1] * valsWeights.ElementAt(0)[0], 4);
+            while (capacity > 0 || valsWeights.Count > 0)
             {
-                if(capacity < valsWeights.Last()[1])
+                if (capacity < valsWeights.Last()[1])
                 {
                     valsWeights.RemoveAt(valsWeights.Count - 1);
                 }
@@ -142,10 +157,13 @@ namespace AlgorithmicToolbox
                 {
                     capacity -= valsWeights.Last()[1];
                     value += valsWeights.Last()[0];
+                    valsWeights.RemoveAt(valsWeights.Count - 1);
                 }
             }
+
             return value;
         }
+
         static void InputForGetOptimalValue()
         {
             //Вводим входные параметры, первый эл массива - кол-во возможныйх вещей, второй эл массива - макс вместимость рюкзака.
@@ -159,12 +177,14 @@ namespace AlgorithmicToolbox
             for (int i = 0; i < init[0]; i++)
             {
                 var row = Console.ReadLine().Split(' ').Select(s => int.Parse(s)).ToArray();
-                valsWeights.Add(new[] { row[0], row[1] });
+                valsWeights.Add(new[] {row[0], row[1]});
             }
+
             CompareListItems cmpr = new CompareListItems();
-            valsWeights.Sort(cmpr);            
-            Console.WriteLine(getOptimalValue(capacity, valsWeights));
+            valsWeights.Sort(cmpr);
+            Console.WriteLine(GetOptimalValue(capacity, valsWeights));
         }
+
         class CompareListItems : IComparer<int[]>
         {
             public int Compare(int[] x, int[] y)
@@ -175,10 +195,8 @@ namespace AlgorithmicToolbox
                 }
 
                 // CompareTo() method 
-                return (x[0]/x[1]).CompareTo(y[0]/y[1]);
-
+                return (x[0] / x[1]).CompareTo(y[0] / y[1]);
             }
         }
     }
-
 }
