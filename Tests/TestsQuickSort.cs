d0976e19f17b09d0b50aa71183c2e05fb5861cc8
elemntry <xarradex@gmail.lcom>
2020-07-28 19:04:34 +0500

    add tests for quick sort

---
+++ Tests/TestsQuickSort.cs
@@ -0,0 +1,28 @@
+﻿using AlgorithmicToolbox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace Tests
+{
+    public class TestsQuickSort
+    {
+        [Fact]
+        public void TestsQuickSort1()
+        {
+            int[] arr = new int[] { 1, 5, 8, 12, 13 };
+
+            Program.QuickSort(arr, 0, arr.Length - 1);
+            Assert.Equal("1, 5, 8, 12, 13", string.Join(", ", arr));
+        }
+        [Fact]
+        public void TestsQuickSort2()
+        {
+            int[] arr = new int[] { 12, 13, 8, 1, 5 };
+
+            Program.QuickSort(arr, 0, arr.Length - 1);
+            Assert.Equal("1, 5, 8, 12, 13", string.Join(", ", arr));
+        }
+    }
+}
