43a2982f8c6e3160b36a7bf5ba12c71287eca80c
elemntry <xarradex@gmail.lcom>
2020-07-28 19:04:50 +0500

    add tests for Majority element

---
+++ Tests/TestsMajorityElement.cs
@@ -0,0 +1,35 @@
+﻿using AlgorithmicToolbox;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+namespace Tests
+{
+    public class TestsMajorityElement
+    {
+        [Fact]
+        public void TestsMajorityElement1()
+        {
+            int[] arr = new int[] { 1, 1, 8, 12, 1 };
+
+            int result = Program.MajorityElement(arr);
+            Assert.Equal(1, result);
+        }        
+        [Fact]
+        public void TestsMajorityElement2()
+        {
+            int[] arr = new int[] { 1, 5, 8, 12, 1 };
+
+            int result = Program.MajorityElement(arr);
+            Assert.Equal(0, result);
+        }
+        [Fact]
+        public void TestsMajorityElement3()
+        {
+            int[] arr = new int[] { 1, 2, 60, 8, 12, 12, 12, 12, 12 };
+
+            int result = Program.MajorityElement(arr);
+            Assert.Equal(1, result);
+        }
+    }
+}
