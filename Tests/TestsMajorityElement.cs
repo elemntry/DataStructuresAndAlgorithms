3b40268b89bd186c0f1c10266cb4eb0a4a7ddba9
elemntry <xarradex@gmail.com>
2020-07-30 22:44:38 +0500

    fix test naming

--- Tests/TestsMajorityElement.cs
+++ Tests/TestsMajorityElement.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using Xunit;
+
 namespace Tests
 {
     public class TestsMajorityElement
@@ -11,25 +12,34 @@ namespace Tests
         [Fact]
         public void TestsMajorityElement1()
         {
-            int[] arr = new int[] { 1, 1, 8, 12, 1 };
+            int[] arr = new int[] {1, 1, 8, 12, 1};
 
-            int result = Program.MajorityElementVariant3(arr);
+            int result = Program.MajorityElement(arr);
             Assert.Equal(1, result);
-        }        
+        }
+
         [Fact]
         public void TestsMajorityElement2()
         {
-            int[] arr = new int[] { 1, 5, 8, 12, 1 };
+            int[] arr = new int[] {1, 5, 8, 12, 1};
 
-            int result = Program.MajorityElementVariant3(arr);
+            int result = Program.MajorityElement(arr);
             Assert.Equal(0, result);
         }
+
         [Fact]
         public void TestsMajorityElement3()
         {
-            int[] arr = new int[] { 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12 };
+            int[] arr = new int[]
+            {
+                1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2,
+                60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60,
+                8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8,
+                12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12, 1, 2, 60, 8, 12,
+                12, 12, 12, 12, 1, 2, 60, 8, 12, 12, 12, 12, 12
+            };
 
-            int result = Program.MajorityElementVariant3(arr);
+            int result = Program.MajorityElement(arr);
             Assert.Equal(1, result);
         }
     }
